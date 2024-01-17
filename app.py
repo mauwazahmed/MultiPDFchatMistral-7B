05687e5371b1a5a983bea608d2f1569166730a87
mauwazahmed <56031816+mauwazahmed@users.noreply.github.com>
2024-01-17 11:33:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.llms import LlamaCpp
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain_community.document_loaders import PyPDFLoader
 import os
